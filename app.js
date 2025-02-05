743fbac73933b577aba01c4f6e187865e9487fe8
KharytonchykRaman <157610424+KharytonchykRaman@users.noreply.github.com>
2025-02-05 23:18:04 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ class ToDoList {
     return false;
   }
 
-  getToDoById(toDoId){
+  getToDoById(toDoId) {
     for (let i = 0; i < this.todos.length; i++) {
       if (this.todos[i].id === toDoId) {
         return this.todos[i];
@@ -26,7 +26,7 @@ class ToDoList {
     return false;
   }
 
-  replaceToDoById(id, todo){
+  replaceToDoById(id, todo) {
     for (let i = 0; i < this.todos.length; i++) {
       if (this.todos[i].id === id) {
         this.todos.splice(i, 1, todo);
@@ -78,41 +78,41 @@ async function inputCurrency() {
 inputTemperature();
 inputCurrency();
 
-function openModalWindow() {
+function openCreateToDoModal() {
   creationTodoWindow.showModal();
 }
 
-function openEditModalWindow() {
+function openEditToDoModal() {
   editTodoWindow.showModal();
 }
 
-function confirmEdit(id){
-  const todo = getElementById(id);
-
-    const title = document.getElementById("editToDo-name").value;
-    const description = document.getElementById("editToDo-description").value;
-    const deadline = document.getElementById("editToDo-date").value;
-    const tags = document.getElementById("editToDo-tags").value;
-    const status = document.getElementById("editToDo-status").value;
-
-    todo.title = title;
-    todo.description = description;
-    todo.deadline = deadline;
-    todo.tags = tags;
-    todo.status = status;
-    todo.updatedAt = new Date();
-    todo.history.push({action:"updated", timestamp: updatedAt})
-    const todoList = getToDoList();
-    todoList.replaceToDoById(id,todo);
-    closeEditModalWindow();
-}
-
-function closeCreationModalWindow() {
+function closeCreateToDoModal() {
   creationTodoWindow.close();
 }
 
-function closeEditModalWindow() {
+function closeEditToDoModal() {
   editTodoWindow.close();
+}
+
+function getToDoList() {
+  if (localStorage.getItem(LOCAL_STORAGE_KEY) === null) {
+    const newToDoList = new ToDoList([]);
+    uploadToDoListToLocalStorage(newToDoList);
+    return newToDoList;
+  }
+
+  const toDoList = new ToDoList(
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)).todos
+  );
+  return toDoList;
+}
+
+function getToDos() {
+  return getToDoList().todos;
+}
+
+function uploadToDoListToLocalStorage(toDoList) {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(toDoList));
 }
 
 function validateNewToDo() {
@@ -140,144 +140,206 @@ function validateNewToDo() {
   return true;
 }
 
+function validateEditedToDo() {
+  if (document.getElementById("editToDo-name").value.length < 1) {
+    alert("empty name");
+    return false;
+  } else if (document.getElementById("editToDo-date").value.length < 1) {
+    alert("empty date");
+    return false;
+  } else if (document.getElementById("editToDo-tags").value.length < 1) {
+    alert("empty tags");
+    return false;
+  }
+
+  const selectedDate = new Date(document.getElementById("editToDo-date").value);
+  const today = new Date();
+
+  today.setHours(0, 0, 0, 0);
+
+  if (selectedDate < today) {
+    alert("wrong deadline");
+    return false;
+  }
+
+  return true;
+}
+
 function generateUniqueId() {
   return Date.now() - Math.floor(Math.random() * 1000);
 }
 
-function getToDoList(){
-  if (localStorage.getItem(LOCAL_STORAGE_KEY) === null) {
-    const newToDoList = new ToDoList([]);
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newToDoList));
-    return newToDoList;
-  }
-
-  const toDoList = new ToDoList(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)).todos);
-  return toDoList;
-}
-
-function getToDos(){
-  return getToDoList().todos;
-}
-
 function createNewToDo() {
-  if (validateNewToDo()) {
-    const id = generateUniqueId();
-    const title = document.getElementById("newToDo-name").value;
-    const description = document.getElementById("newToDo-description").value;
-    const deadline = document.getElementById("newToDo-date").value;
-    const tags = document.getElementById("newToDo-tags").value;
-    const status = document.getElementById("newToDo-status").value;
-    const createdAt = new Date();
-    const updatedAt = createdAt;
-    const history = [{ action: "created", timestamp: createdAt }];
+  const id = generateUniqueId();
+  const title = document.getElementById("newToDo-name").value;
+  const description = document.getElementById("newToDo-description").value;
+  const deadline = document.getElementById("newToDo-date").value;
+  const tags = document.getElementById("newToDo-tags").value;
+  const status = document.getElementById("newToDo-status").value;
+  const createdAt = new Date();
+  const updatedAt = createdAt;
+  const history = [{ action: "created", timestamp: createdAt }];
 
-    let newToDo = {
-      id: id,
-      title: title,
-      description: description,
-      deadline: deadline,
-      tags: tags,
-      status: status,
-      createdAt: createdAt,
-      updatedAt: updatedAt,
-      history: history,
-    };
+  let newToDo = {
+    id: id,
+    title: title,
+    description: description,
+    deadline: deadline,
+    tags: tags,
+    status: status,
+    createdAt: createdAt,
+    updatedAt: updatedAt,
+    history: history,
+  };
+
+  return newToDo;
+}
+
+function renderNewToDo() {
+  if (validateNewToDo()) {
+    let newToDo = createNewToDo();
+    renderToDo(newToDo);
+
     const toDoList = getToDoList();
     toDoList.addToDo(newToDo);
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(toDoList));
-    closeCreationModalWindow();
-    renderToDo(newToDo);
+    uploadToDoListToLocalStorage(toDoList);
+
+    closeCreateToDoModal();
     showCustomToast("Задача добавлена!");
   }
 }
 
-
 function editToDo(id) {
+  if (validateEditedToDo()) {
+    const todo = getToDoList().getToDoById(id);
+
+    const title = document.getElementById("editToDo-name").value;
+    const description = document.getElementById("editToDo-description").value;
+    const deadline = document.getElementById("editToDo-date").value;
+    const tags = document.getElementById("editToDo-tags").value;
+    const status = document.getElementById("editToDo-status").value;
+
+    todo.title = title;
+    todo.description = description;
+    todo.deadline = deadline;
+    todo.tags = tags;
+    todo.status = status;
+    todo.updatedAt = new Date();
+    todo.history.push({ action: "updated", timestamp: updatedAt });
+
+    const todoList = getToDoList();
+    todoList.replaceToDoById(id, todo);
+    closeEditToDoModal();
+  }
+}
+
+function initEditToDoModal(id) {
   const todo = getToDoList().getToDoById(id);
 
-  openEditModalWindow();
+  openEditToDoModal();
   document.getElementById("editToDo-name").value = todo.title;
   document.getElementById("editToDo-description").value = todo.description;
   document.getElementById("editToDo-date").value = todo.deadline;
   document.getElementById("editToDo-tags").value = todo.tags;
   document.getElementById("editToDo-status").value = todo.status;
-  acceptEditButton.addEventListener("click", function(){confirmEdit(id); renderToDosArray();})
+  acceptEditButton.addEventListener("click", function () {
+    editToDo(id);
+  });
 }
 
 function deleteToDo(id) {
-    const toDoList = getToDoList();
-    toDoList.deleteToDoById(id);
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(toDoList));
+  const toDoList = getToDoList();
+  toDoList.deleteToDoById(id);
+  uploadToDoListToLocalStorage(toDoList);
 }
 
-function confirmDeleteToDo(id){
-  let confirmation = confirm("Вы уверены, что хотите удалить задачу?");
+function confirmDeleteToDo(id) {
+  const confirmation = confirm("Вы уверены, что хотите удалить задачу?");
   if (confirmation) {
     deleteToDo(id);
-    showCustomToast("Задача удалена!")
+    showCustomToast("Задача удалена!");
+    renderLocalStorageToDosArray();
   }
 }
 
-function initToDo(todo) {
-  const template = document.getElementById("template");
+function initToDoIntoTemplate(todo) {
   const item = template.content.cloneNode(true);
+
   item.querySelector(".todo-name").innerHTML = todo.title;
   item.querySelector(".todo-description").innerHTML = todo.description;
   item.querySelector(".todo-deadline").innerHTML = todo.deadline;
   item.querySelector(".todo-tags").innerHTML = todo.tags;
   item.querySelector(".todo-status").innerHTML = todo.status;
-  const deleteButton = item.querySelector(".delete-todo");
-  const editButton = item.querySelector(".edit-todo");
-  deleteButton.addEventListener("click",function(){confirmDeleteToDo(todo.id);  renderToDosArray();} );
-  editButton.addEventListener("click",function(){editToDo(todo.id);  renderToDosArray();} );
+  item
+    .querySelector(".delete-todo-button")
+    .addEventListener("click", function () {
+      confirmDeleteToDo(todo.id);
+    });
+  item
+    .querySelector(".edit-todo-button")
+    .addEventListener("click", function () {
+      initEditToDoModal(todo.id);
+    });
+
   return item;
 }
 
 function renderToDo(todo) {
-  const item = initToDo(todo);
-  const todoContainer = document.getElementById("todo-container");
+  const item = initToDoIntoTemplate(todo);
   todoContainer.append(item);
 }
 
-function clearToDoContainer(){
-  const todoContainer = document.getElementById("todo-container");
-  todoContainer.innerHTML = '';
+function clearToDoContainer() {
+  todoContainer.innerHTML = "";
 }
 
-function renderToDosArray() {
+function renderLocalStorageToDosArray() {
   clearToDoContainer();
+
   const todosArray = getToDos();
   for (let i = 0; i < todosArray.length; i++) {
     renderToDo(todosArray[i]);
   }
 }
 
+function renderToDosArray(toDosArray) {
+  clearToDoContainer();
+
+  for (let i = 0; i < toDosArray.length; i++) {
+    renderToDo(toDosArray[i]);
+  }
+}
+
 function showCustomToast(message) {
   toastElement.textContent = message;
 
-  toastElement.classList.remove('hidden');
+  toastElement.classList.remove("hidden");
 
   setTimeout(() => {
-    toastElement.classList.add('hidden');
-  }, 3000); 
+    toastElement.classList.add("hidden");
+  }, 3000);
 }
 
 const createTodoButton = document.getElementById("createTodo-button");
-const cancelCreationButton = document.getElementById("closeModal");
-const acceptCreationButton = document.getElementById("acceptCreation");
+const cancelCreateToDoButton = document.getElementById("closeModal");
+const acceptCreateToDoButton = document.getElementById("acceptCreation");
 
 const cancelEditButton = document.getElementById("closeEditModal");
 const acceptEditButton = document.getElementById("acceptEdit");
 
-const creationTodoWindow = document.getElementById("createTodo-dialog");
-const editTodoWindow = document.getElementById("editTodo-dialog");
+const createTodoModal = document.getElementById("createTodo-dialog");
+const editTodoModal = document.getElementById("editTodo-dialog");
 
-const toastElement = document.getElementById('toast');
+const toastElement = document.getElementById("toast");
 
-createTodoButton.addEventListener("click", openModalWindow);
-cancelCreationButton.addEventListener("click", closeCreationModalWindow);
-acceptCreationButton.addEventListener("click", createNewToDo);
+const todoContainer = document.getElementById("todo-container");
 
-cancelEditButton.addEventListener("click", closeEditModalWindow);
+const template = document.getElementById("template");
 
-renderToDosArray();
+createTodoButton.addEventListener("click", openCreateToDoModal);
+cancelCreateToDoButton.addEventListener("click", closeCreateToDoModal);
+acceptCreateToDoButton.addEventListener("click", renderNewToDo);
+
+cancelEditButton.addEventListener("click", closeEditToDoModal);
+
+renderLocalStorageToDosArray();
